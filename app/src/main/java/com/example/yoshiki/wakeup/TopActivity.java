d60d07e12c9b672a824bdd689dd645b12b83e8e5
yoshiki <0114yoshiki@gmail.cpm>
2015-11-03 19:41:25 +0900

    再commit

--- app/src/main/java/com/example/yoshiki/wakeup/TopActivity.java
+++ app/src/main/java/com/example/yoshiki/wakeup/TopActivity.java
@@ -1,7 +1,14 @@
 package com.example.yoshiki.wakeup;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by yoshiki on 2015/10/25.
  */
-public class TopActivity {
+public class TopActivity extends Activity{
+    public void onCreate(Bundle saveInstance){
+        super.onCreate(saveInstance);
+        setContentView(R.layout.activity_top);
+    }
 }
