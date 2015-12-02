5e43b0a7f3c68a6c3cb4b74769b9477a3dc49273
yoshiki <0114yoshiki@gmail.cpm>
2015-12-02 18:42:05 +0900

    ボタン配置変更

--- app/src/main/java/com/example/yoshiki/wakeup/TopActivity.java
+++ app/src/main/java/com/example/yoshiki/wakeup/TopActivity.java
@@ -72,13 +72,14 @@ public class TopActivity extends Activity implements Runnable {
                     evalu_check=false;
                 }
 
-                /*
-                デモ用。常に評価画面に遷移
+/*
+
+                //デモ用。常に評価画面に遷移
                 Intent intent_evalu =new Intent(this,EvaluationActivity.class);
                 startActivity(intent_evalu);
                 evalu_check=false;
                 break;
-                */
+*/
         }
         finish();
         return true;
